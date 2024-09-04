112eaaaca222cf17139d08ca74a887e60ac5690f
MNS <parsamanes@yahoo.com>
2024-09-04 10:27:51 +0300

    added the studymd things

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./src/main.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'dark-cyan' : 'hsl(158, 36%, 37%)',
+        'cream' : 'hsl(30, 38%, 92%)',
+        'very-dark-blue' : 'hsl(212, 21%, 14%)',
+        'dark-grayish-blue' : 'hsl(228, 12%, 48%)',
+      },
+      fontSize:{
+        'paragraph' : '14px'
+      },
+    },
   },
   plugins: [],
 }
+
